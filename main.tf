fe55764db514166b74dc5e7f9f07acd4a80902de
Cosmin Briscaru <cosmin@coremaker.io>
2023-06-26 17:14:07 +0100

    fix org access

--- main.tf
+++ main.tf
@@ -18,7 +18,19 @@ resource "tfe_team" "main" {
     name         = each.key
     organization = var.tfe_organization_name
     visibility   = each.value["visibility"]
-    organization_access = each.value["organization_access"]
+    organization_access {
+        read_workspaces = lookup(each.value["organization_access"], "read_workspaces", null)
+        read_projects = lookup(each.value["organization_access"], "read_projects", null)
+        manage_policies = lookup(each.value["organization_access"], "manage_policies", null)
+        manage_policy_overrides = lookup(each.value["organization_access"], "manage_policy_overrides", null)
+        manage_workspaces = lookup(each.value["organization_access"], "manage_workspaces", null)
+        manage_vcs_settings = lookup(each.value["organization_access"], "manage_vcs_settings", null)
+        manage_providers = lookup(each.value["organization_access"], "manage_providers", null)
+        manage_modules = lookup(each.value["organization_access"], "manage_modules", null)
+        manage_run_tasks = lookup(each.value["organization_access"], "manage_run_tasks", null)
+        manage_projects = lookup(each.value["organization_access"], "manage_projects", null)
+        manage_membership =lookup(each.value["organization_access"], "manage_membership", null)
+    }
 }
 
 resource "tfe_team_organization_members" "main" {
